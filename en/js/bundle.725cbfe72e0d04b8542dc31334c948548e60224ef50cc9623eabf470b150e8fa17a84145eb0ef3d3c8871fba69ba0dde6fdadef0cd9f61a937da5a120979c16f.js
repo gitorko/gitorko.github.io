d9092dc64e5034f1b95566af66eabb0b8d44d882
gitorko <gitorko@users.noreply.github.com>
2023-09-21 10:34:50 +0000

    deploy: d2fba22ae90a385877df71d8d4496bca9e794ce6

--- en/js/bundle.725cbfe72e0d04b8542dc31334c948548e60224ef50cc9623eabf470b150e8fa17a84145eb0ef3d3c8871fba69ba0dde6fdadef0cd9f61a937da5a120979c16f.js
+++ en/js/bundle.725cbfe72e0d04b8542dc31334c948548e60224ef50cc9623eabf470b150e8fa17a84145eb0ef3d3c8871fba69ba0dde6fdadef0cd9f61a937da5a120979c16f.js
@@ -4,8 +4,10 @@ const featuredImageClass = 'image_featured';
 const imageScalableClass = 'image-scalable';
 const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
-const imageAltClass = 'img_alt'
+const imageAltClass = 'img_alt';
 
+
+const defaultSiteLanguage = 'en';
 const baseURL = 'https://gitorko.github.io/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
@@ -881,6 +883,9 @@ function fileClosure(){
         // Insert caption
         image.insertAdjacentHTML('afterend', desc.outerHTML);
       }
+
+      // Persist modified alt to image element
+      image.alt = alt
     });
 
     hljs.initHighlightingOnLoad();
